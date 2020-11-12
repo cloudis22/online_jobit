5f236bc926b34ba08c6897192a2f3fc6169e7e8f
cloudis22 <68263921+cloudis22@users.noreply.github.com>
2020-11-12 18:00:36 +0200

    Update serveria.lua

--- serveria.lua
+++ serveria.lua
@@ -8,28 +8,28 @@ ESX.RegisterServerCallback('cloudis_scoreboard', function(source, cb)
   
   local xPlayers = ESX.GetPlayers()
 
-	EMSConnected = 0
-	PoliceConnected = 0
-	TaxiConnected = 0
-	MechanicConnected = 0
+	Lanssit = 0
+	Poliisit = 0
+	Taxit = 0
+	Mekaanikot = 0
 
 	for i=1, #xPlayers, 1 do
 		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
 		if xPlayer.job.name == 'ambulance' then
-			EMSConnected = EMSConnected + 1
+			Lanssit = Lanssit + 1
 		end		
 		if xPlayer.job.name == 'police' then
-			PoliceConnected = PoliceConnected + 1
+			Poliisit = Poliisit + 1
 		end	
 		if xPlayer.job.name == 'taxi' then
-			TaxiConnected = TaxiConnected + 1
+			Taxit = Taxit + 1
 		end	
 		if xPlayer.job.name == 'mechanic' then
-			MechanicConnected = MechanicConnected + 1
+			Mekaanikot = Mekaanikot + 1
 		end
 	end
   
   
-cb(EMSConnected, PoliceConnected, TaxiConnected, MechanicConnected)
+cb(Lanssit, Poliisit, Taxit, Mekaanikot)
 
 end)

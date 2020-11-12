d16f41025000db349d9b7aa97252664f2203abb7
cloudis22 <68263921+cloudis22@users.noreply.github.com>
2020-11-12 18:02:43 +0200

    Update clienttia.lua

--- clienttia.lua
+++ clienttia.lua
@@ -7,19 +7,19 @@ end)
 
 local id = GetPlayerServerId(PlayerId())
 
-emsonline = 0
-policeonline = 0
-taxionline = 0
-mechaniconline = 0
+LanssitPaikalla = 0
+PoliisitPaikalla = 0
+TaxitPaikalla = 0
+MekaanikotPaikalla = 0
 
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(10000)
-		ESX.TriggerServerCallback('cloudis_scoreboard', function(ems, police, taxi, mechanic)
-			emsonline    = ems
-			policeonline = police
-			taxionline   = taxi 
-			mechaniconline  = mechanic 
+		ESX.TriggerServerCallback('cloudis_scoreboard', function(lanssi, poliisi, taxi, meksu)
+			LanssitPaikalla    = lanssi
+			PoliisitPaikalla = poliisi
+			TaxitPaikalla   = taxi 
+			MekaanikotPaikalla  = meksu 
 		end)
 	end
 end)
@@ -28,7 +28,7 @@ end)
  	while true do
  		Citizen.Wait(0)
  		if IsControlPressed(0, 178) then
-		ESX.ShowNotification("Poliiseja: ~r~" .. policeonline .. "~s~\nEnsihoitajia: ~r~" .. emsonline .. "~s~\nTaxeja: ~r~" .. taxionline .. "~s~\nMekaanikkoja: ~r~" .. mechaniconline .. "~s~\n\nSinun ID: ~r~" .. id )
+		ESX.ShowNotification("Poliiseja: ~r~" .. PoliisitPaikalla .. "~s~\nEnsihoitajia: ~r~" .. LanssitPaikalla .. "~s~\nTaxeja: ~r~" .. TaxitPaikalla .. "~s~\nMekaanikkoja: ~r~" .. MekaanikotPaikalla .. "~s~\n\nSinun ID: ~r~" .. id )
  		end
  	end
  end)
